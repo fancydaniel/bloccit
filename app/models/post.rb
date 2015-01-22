6afc52e3d0c0a9c151d94619cc6578ec34e2fc83
Daniel Uribe <duribe8@gmail.com>
2015-01-21 20:57:18 -0800

    Voting added to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,7 +19,7 @@ class Post < ActiveRecord::Base
       votes.sum(:value)  
     end
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -33,6 +33,12 @@ class Post < ActiveRecord::Base
   # def markdown_body
   #   render_as_markdown body
   # end
-  
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age_in_days
+
+    update_attribute(:rank, new_rank)
+  end  
 
 end
