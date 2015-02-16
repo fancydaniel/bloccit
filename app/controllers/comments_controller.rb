47a1a7510ee1ba7579db5950d64fb80cc547ac3d
Daniel Uribe <duribe8@gmail.com>
2015-02-15 16:58:44 -0800

    Creating with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,33 @@ class CommentsController < ApplicationController
   respond_to :html, :js
 
  def create
-    @comment = current_user.comments.build(params.require(:comment).permit(:body))
-    post = Post.find(params[:post_id])
-    @comment.post = post
-    # topic = Topic.find(params[:topic_id])
-    if @comment.save
-      flash[:notice] = "Comment was saved."
-      redirect_to [post.topic, post]  # Does this need to be changed? Do we add @comments?
-    end
+  @post = Post.find(params[:post_id])
+  @comments = @post.comments
+
+  @comment = current_user.comments.build( comment_params )
+  @comment.post = @post
+    @new_comment = Comment.new
+
+  authorize @comment
+
+  if @comment.save
+    flash[:notice] = "Comment was created."
+  else
+    flash[:error] = "There was an error saving the comment. Please try again."
+  end
+
+  respond_with(@comment) do |format|
+    format.html { redirect_to [@post.topic, @post] }
+  end
+
+    # @comment = current_user.comments.build(params.require(:comment).permit(:body))
+    # post = Post.find(params[:post_id])
+    # @comment.post = post
+    # # topic = Topic.find(params[:topic_id])
+    # if @comment.save
+    #   flash[:notice] = "Comment was saved."
+    #   redirect_to [post.topic, post]  # Does this need to be changed? Do we add @comments?
+    # end
   end
 
   def destroy
@@ -30,6 +49,12 @@ class CommentsController < ApplicationController
     respond_with(@comment) do |format|
       format.html { redirect_to [post.topic, post] }
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 
 end
